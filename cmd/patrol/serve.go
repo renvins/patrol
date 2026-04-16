498b11c7989428986c1078ab4e6ca62b282b5ca3
renvins <vincenzoreinaa05@gmail.com>
2026-04-16 22:40:13 +0200

    Add gRPC support

--- cmd/patrol/serve.go
+++ cmd/patrol/serve.go
@@ -11,12 +11,14 @@ import (
 	"github.com/renvins/patrol/internal/api"
 	"github.com/renvins/patrol/internal/config"
 	"github.com/renvins/patrol/internal/engine"
+	"github.com/renvins/patrol/internal/grpcserver"
 	"github.com/renvins/patrol/internal/prometheus"
 	"github.com/spf13/cobra"
 )
 
 var configPath string
 var listenAddr string
+var grpcAddr string
 
 var serveCmd = &cobra.Command{
 	Use:   "serve",
@@ -26,7 +28,8 @@ var serveCmd = &cobra.Command{
 
 func init() {
 	serveCmd.Flags().StringVar(&configPath, "config", "patrol.yml", "path to SLO config file")
-	serveCmd.Flags().StringVar(&listenAddr, "listen", ":8080", "address to listen on")
+	serveCmd.Flags().StringVar(&listenAddr, "listen", ":8080", "REST API listen address")
+	serveCmd.Flags().StringVar(&grpcAddr, "grpc", ":9090", "gRPC listen address")
 }
 
 func runServe(cmd *cobra.Command, args []string) error {
@@ -36,15 +39,17 @@ func runServe(cmd *cobra.Command, args []string) error {
 		os.Exit(1)
 	}
 
-	prometheusClient := prometheus.New("http://localhost:9090")
+	prometheusClient := prometheus.New("http://localhost:9091")
 	eng := engine.New(prometheusClient, sloConfig, 30*time.Second)
-	server := api.New(eng)
+	restServer := api.New(eng)
+	grpcServer := grpcserver.New(eng)
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	engineErrChan := make(chan error, 1)
-	serverErrChan := make(chan error, 1)
+	restErrChan := make(chan error, 1)
+	grpcErrChan := make(chan error, 1)
 
 	go func() {
 		if err := eng.Run(ctx); err != nil {
@@ -53,18 +58,26 @@ func runServe(cmd *cobra.Command, args []string) error {
 	}()
 
 	go func() {
-		if err := server.Run(ctx, listenAddr); err != nil {
-			serverErrChan <- err
+		if err := restServer.Run(ctx, listenAddr); err != nil {
+			restErrChan <- err
 		}
 	}()
 
-	slog.Info("patrol started", "listen", listenAddr, "config", configPath)
+	go func() {
+		if err := grpcServer.Run(ctx, grpcAddr); err != nil {
+			grpcErrChan <- err
+		}
+	}()
+
+	slog.Info("patrol started", "rest", listenAddr, "grpc", grpcAddr, "config", configPath)
 
 	select {
 	case err := <-engineErrChan:
 		slog.Error("engine error", "error", err)
-	case err := <-serverErrChan:
-		slog.Error("server error", "error", err)
+	case err := <-restErrChan:
+		slog.Error("rest server error", "error", err)
+	case err := <-grpcErrChan:
+		slog.Error("grpc server error", "error", err)
 	}
 	return nil
 }
--- internal/grpcserver/server.go
+++ internal/grpcserver/server.go
@@ -2,26 +2,73 @@ package grpcserver
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"time"
 
 	"github.com/renvins/patrol/internal/engine"
 	patrolv1 "github.com/renvins/patrol/proto/patrol/v1"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Server struct {
-	patrolv1.UnimplementedPatrolServiceServer // embed this
-	engine                                    *engine.Engine
+	patrolv1.UnimplementedPatrolServiceServer
+	engine *engine.Engine
 }
 
 func New(e *engine.Engine) *Server {
 	return &Server{engine: e}
 }
 
-func (s *Server) GetStatus(ctx context.Context, req *patrolv1.GetStatusRequest) (*patrolv1.GetStatusResponse, error) {
-	budgetStatus, ok := s.engine.GetStatus(req.SloName)
+func (s *Server) GetStatus(_ context.Context, req *patrolv1.GetStatusRequest) (*patrolv1.GetStatusResponse, error) {
+	bs, ok := s.engine.GetStatus(req.SloName)
 	if !ok {
 		return nil, status.Errorf(codes.NotFound, "SLO %q not found", req.SloName)
 	}
+	return &patrolv1.GetStatusResponse{
+		SloName:         bs.SLOName,
+		BurnRate:        bs.BurnRate,
+		BudgetConsumed:  bs.BudgetConsumed,
+		BudgetRemaining: bs.BudgetRemaining,
+		ErrorRate:       bs.ErrorRate,
+		UpdatedAt:       bs.UpdatedAt.Format(time.RFC3339),
+	}, nil
+}
 
+func (s *Server) GetGate(_ context.Context, req *patrolv1.GetGateRequest) (*patrolv1.GetGateResponse, error) {
+	bs, ok := s.engine.GetStatus(req.SloName)
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "SLO %q not found", req.SloName)
+	}
+	if bs.BudgetRemaining > 0 {
+		return &patrolv1.GetGateResponse{Allow: true}, nil
+	}
+	return &patrolv1.GetGateResponse{Allow: false, Reason: "error budget exhausted"}, nil
+}
+
+func (s *Server) Run(ctx context.Context, addr string) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %w", addr, err)
+	}
+
+	grpcSrv := grpc.NewServer()
+	patrolv1.RegisterPatrolServiceServer(grpcSrv, s)
+
+	errChan := make(chan error, 1)
+	go func() {
+		if err := grpcSrv.Serve(lis); err != nil {
+			errChan <- err
+		}
+	}()
+
+	select {
+	case err := <-errChan:
+		return fmt.Errorf("grpc server error: %w", err)
+	case <-ctx.Done():
+		grpcSrv.GracefulStop()
+		return nil
+	}
 }
